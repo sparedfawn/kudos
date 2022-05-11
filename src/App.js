f8b0f895033fc6865ce8b01ceb11035205c0e1f6
Krzysztof Kalinowski <k.kalinowski2000@gmail.com>
2022-05-11 09:48:52 +0200

    initialized project with webpack config

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+    return <h1>App {new Date().toDateString()}</h1>;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+require('file-loader?name=[name].[ext]!./index.html');
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import "./index.scss";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
