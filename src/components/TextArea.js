68567e9bb0b1f79cd9ef33cdd23f909b2eaf914e
Krzysztof Kalinowski <k.kalinowski2000@gmail.com>
2022-05-12 18:13:02 +0200

    added mentions with @ sign in textarea

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,33 +1,58 @@
-import React, { useState } from "react";
+import React, { useCallback, useState, useMemo } from "react";
 import { EditorState } from "draft-js";
 import Editor from "@draft-js-plugins/editor";
 import createEmojiPlugin from "@draft-js-plugins/emoji";
 import createHashtagPlugin from "@draft-js-plugins/hashtag";
+import createMentionPlugin, { defaultSuggestionsFilter } from "@draft-js-plugins/mention";
+
 import "@draft-js-plugins/emoji/lib/plugin.css";
 import "@draft-js-plugins/hashtag/lib/plugin.css";
+import "@draft-js-plugins/mention/lib/plugin.css";
 
 import GifIcon from "../icons/gif.svg";
 import AttachmentIcon from "../icons/attachment.svg";
-
-const hashtagPlugin = createHashtagPlugin();
-const emojiPlugin = createEmojiPlugin();
-const { EmojiSuggestions, EmojiSelect } = emojiPlugin;
+import mentions from "./mentions";
 
 const TextArea = () => {
+    const [open, setOpen] = useState(false);
+    const [suggestions, setSuggestions] = useState(mentions);
+
     const [editorValue, setEditorValue] = useState(() => EditorState.createEmpty());
+
+    const { plugins, EmojiSuggestions, EmojiSelect, MentionSuggestions } = useMemo(() => {
+        const mentionPlugin = createMentionPlugin();
+        const hashtagPlugin = createHashtagPlugin();
+        const emojiPlugin = createEmojiPlugin({ useNativeArt: false }); 
+
+        const { MentionSuggestions } = mentionPlugin;
+        const { EmojiSuggestions, EmojiSelect } = emojiPlugin;
+        const plugins = [mentionPlugin, hashtagPlugin, emojiPlugin];
+
+        return { plugins, EmojiSuggestions, EmojiSelect, MentionSuggestions };
+    }, []);
 
     const handleTextEditorValueChange = (value) => {
         setEditorValue(value);
     };
 
+    const onOpenChange = useCallback((open) => {
+        setOpen(open);
+    }, []);
+
+    const onSearchChange = useCallback(({ value }) => {
+        setSuggestions(defaultSuggestionsFilter(value, mentions));
+    }, []);
+
     return (
         <div>
-            <Editor
-                editorState={editorValue}
-                onChange={handleTextEditorValueChange}
-                plugins={[emojiPlugin, hashtagPlugin]}
-            />
+            <Editor editorState={editorValue} onChange={handleTextEditorValueChange} plugins={plugins} />
             <EmojiSuggestions />
+            <MentionSuggestions
+                open={open}
+                onOpenChange={onOpenChange}
+                suggestions={suggestions}
+                onSearchChange={onSearchChange}
+            />
             <img src={GifIcon} />
             <EmojiSelect />
             <img src={AttachmentIcon} />
